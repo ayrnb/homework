ced448800a17537dae6bd7adcb0100172ab70528
cy <1348987670@qq.com>
2024-04-17 20:28:19 +0800

    第二次提交”

--- draw.py
+++ draw.py
@@ -2,11 +2,12 @@ import pickle
 import numpy as np
 from matplotlib import pyplot as plt
 
-lr_list = [0.1, 0.05, 0.01, 0.001]
+
+lr_list = [0.1, 0.01, 0.001, 0.0001]
 color_list = ["b", "g", "r", "k"]
 d_list = []
 for lr in lr_list:
-    with open("output/gd_lr-%f.pickle" % lr, "rb") as f:
+    with open("agd_lr_{:g}.pickle".format(lr), "rb") as f:
         d_list.append(pickle.load(f))
 
 for i in range(len(lr_list)):
